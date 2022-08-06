4dc850b341b3b145256355fd13dd6f1b56d54c42
Mamytova Aziza <aa5414774@gmail.com>
2022-08-06 11:29:40 +0600

    first commit

---
+++ greeting.js
@@ -0,0 +1,10 @@
+
+// let name = prompt("Как вас зовут?","Aziza");
+// alert(`привет ${name} !`);
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// console.log(arr.length);
+//
+// let arr2 = ['black', 'white', 'blue', 'yellow', 'red'];
+// console.log(arr2.length);
+
